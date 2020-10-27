4768d99d01ec60d8fce6678a50ef8e02f66b17b4
Javier Martínez Ruiz <javiermaruiz@hotmail.com>
2020-10-27 11:57:26 +0100

    Cubo

---
+++ src/pruebacubo/Cubo.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacubo;
+
+
+
+
+
+
+
+/**
+ * La clase Cubo diseña objetos geométricos cúbicos
+ siendo todos sus lados iguales. La variable arista indica la dimensión de su
+ * lado y la variable contenido, informa del volumen del contenido del cubo
+ * @author javie
+ */
+public class Cubo {
+    
+  
+    static int contador=0;        // complementael  nomnbre  a los cubos  
+    private double arista;       // el lado del cubo
+    private double contenido;   //representa como de lleno está el cubo
+    private String name;        
+
+       
+    public Cubo(double a, String n){
+            
+        arista=a;
+        contador++;
+        setName(n);
+       
+        
+    }
+
+    
+    public void vuelcaEn(Cubo destino){
+        
+       
+        if(this.getContenido()<=destino.volumen(destino.getArista())){ destino.setContenido(this.getContenido());
+             this.setContenido(0);
+             System.out.println(this.toString());
+             System.out.println(destino.toString());}
+        
+        else if(this.getContenido()>destino.volumen(destino.getArista())){
+        
+            this.setContenido(this.getContenido()-destino.volumen(destino.getArista()));
+            destino.llena(destino.getArista());
+            System.out.println(this.toString());
+            System.out.println(destino.toString());
+        }
+        
+        
+        
+      
+    }
+
+    
+     public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name+String.valueOf(contador);
+    }
+    
+    /**
+     * 
+     * @param a recibe un valor para calcular el volumen del cubo
+     * que representa la máxima capacidad
+     * @return la máxima capacidad del cubo
+     */
+    public double volumen(double a){ 
+        
+        return Math.pow(a, 3);
+    }
+
+    
+    public void llena(double a){
+        
+        contenido=volumen(a);
+    }
+    
+    
+    public void vacia(){
+        
+        if(getContenido()==0) System.out.println("El "+ getName()+ " ya está vacío");
+        else { setContenido(0);
+               System.out.println("El " +getName()+ " ahora está vacío"); 
+               System.out.println(toString());
+        }
+        
+    }
+
+    public double getArista() {
+        return arista;
+    }
+    
+
+    public void setArista(double arista) {
+        this.arista = arista;
+    }
+
+    public double getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(double contenido) {
+        this.contenido = contenido;
+    }
+    
+    @Override
+    public String toString(){
+        
+        return "El " +getName()+ " tiene una capacidad de "+ volumen(arista) +
+                (getContenido()==0? " y ahora está vacío": (getContenido()==volumen(getArista())? 
+                " y ahora está lleno": " y un contenido de "+ getContenido()));
+    }
+         
+
+
+}
---
+++ src/pruebacubo/PruebaCubo.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacubo;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author javie
+ */
+public class PruebaCubo {
+    
+      static Scanner entrada=new Scanner(System.in);
+    
+      public static void main(String[] args) {
+         
+            
+         Cubo cubo=null;   
+         int opcion=-1;
+         
+     
+            do{
+                if(cubo==null){ System.out.println ("Creando un cubo origen: ");
+                 cubo= new Cubo(validarDouble(), "Cubo_origen_"); 
+                  System.out.println(cubo.toString());
+                  
+                 
+                }
+                
+               try{
+                System.out.println();
+                System.out.println("Introduzca una opción");
+                System.out.println("1. Vacíe el cubo:  ");
+                System.out.println("2. Llene el cubo: ");
+                System.out.println("3. Vierta el contenido de un cubo en otro:  ");
+                System.out.println("4. Destruyendo el cubo:  ");
+                System.out.println("0. Salir:  ");
+                System.out.print("--> ");
+                opcion=entrada.nextInt();
+               }catch (InputMismatchException e){ 
+                                                 System.err.println("Error en la opción");
+                                                 entrada.nextLine();}
+            
+               
+             finally {
+                    if (opcion == 0) { 
+                         System.out.println("Cerrando el scanner");
+                       entrada.close(); }
+               }
+               switch(opcion){
+                   
+                   
+                   case 0:   
+                             System.out.println("Au revoir");
+                             break;
+                   
+                   case 1:
+                            cubo.vacia();
+                            break;
+                        
+                   case 2:  
+                            cubo.llena(cubo.getArista());
+                            System.out.println(cubo.toString());
+                            break;
+                                 
+                   case 3:
+                           if(cubo.getContenido()==0)System.out.println("El "+ cubo.getName()+ 
+                                   " está vacío...que quieres volcar?. Primero llene el cubo");
+                           else cubo.vuelcaEn(new Cubo(validarDouble(), "Cubo_Destino_"));
+                           break;
+                        
+                   case 4: 
+                           cubo=null;
+                           System.out.println("Cubo destruido");
+                           break;
+                            
+                            
+                   
+                   default: System.out.println("Escriba un número entre 1 y 4 o 0 para salir");
+                            break;
+                            
+               }
+             
+                 
+
+          
+            }while(opcion!=0);
+                
+            }
+      
+          public static double validarDouble(){
+            
+        
+                        double d=0;                     
+                        boolean  flag=false;        
+                       
+                        
+                             while(!flag){
+                                     try{
+                                         System.out.println("Escriba el lado del cubo");       
+                                         d=entrada.nextDouble();                                                                      
+                                         flag=true;                                            
+                                    }catch(InputMismatchException e){ System.err.println("Error en la inserción");
+                                    entrada.nextLine();}
+                             }
+                             
+                             return d;
+                     
+             }
+    
+}
